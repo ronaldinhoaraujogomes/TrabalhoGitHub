c200923e4d894624d975aad4ee9661047d91af24
Allan Soares <alansoaresnet@gmail.com>
2011-04-12 00:25:11 -0300

    Classe modificada Versão 2.0

---
+++ src/br/una/git/primeiro/TestGitHub.java
@@ -0,0 +1,15 @@
+package br.una.git.primeiro;
+
+public class TestGitHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Trabalho sobre GitHub!");
+		
+		System.out.println("Trabalho motificado por Allan!");
+
+	}
+
+}

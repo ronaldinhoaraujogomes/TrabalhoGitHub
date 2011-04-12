ffd244adc495eba8b4875f0415dce78d380ae6f2
Allan Casa <allaneslomejor@gmail.com>
2011-04-12 00:32:12 -0300

    Uniao do projeto versao 2.0

--- src/br/una/git/primeiro/TestGitHub.java
+++ src/br/una/git/primeiro/TestGitHub.java
@@ -7,8 +7,6 @@ public class TestGitHub {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Trabalho sobre GitHub!");
-		
-		System.out.println("Trabalho motificado por Allan!");
 
 	}
 
